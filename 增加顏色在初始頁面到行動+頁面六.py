012652ad660327ad50d7a8f1f353ce493e965bfe
li-hong-chang <b08303051@g.ntu.edu.tw>
2020-12-17 11:23:09 +0800

    目前集大成，這樣應該可以

--- 增加顏色在初始頁面到行動+頁面六.py
+++ 增加顏色在初始頁面到行動+頁面六.py
@@ -27,10 +27,13 @@ class StartPage(tk.Frame):
         self.createWidgets()
         self.master.title('我的大學')
         self.master.geometry("800x600")
+        self.master.minsize(800, 600)
+        self.master.maxsize(800, 600)
 
     def usr_login(self):
     # 獲取使用者輸入的usr_name和
         usr_name = self.var_usr_name.get()
+        StartPage.name = usr_name
         yy = tkFont.families()
         if usr_name != '':
             self.destroy()
@@ -46,7 +49,7 @@ class StartPage(tk.Frame):
         self.hit = tk.Label(self, text='(你可以不輸入角色名稱看看)', font=('KaiTi', 20)).pack()
         self.var_usr_name = tk.StringVar()
         self.entry_usr_name = tk.Entry(self, textvariable=self.var_usr_name, font=('KaiTi', 20)).pack(pady=10)
-        self.btn_login = tk.Button(self, text='登入', bg='#A877BA', font=('KaiTi', 20), command=self.usr_login)
+        self.btn_login = tk.Button(self, text='登入', bg='#ffcc69', font=('KaiTi', 20), command=self.usr_login)
         self.btn_login.pack(padx=10, pady=10)
 
 class Game(tk.Frame):
@@ -62,25 +65,28 @@ class Game(tk.Frame):
         f1 = tkFont.Font(size = 30, family = "Courier New")
         f2 = tkFont.Font(size = 25, family = "Courier New")
         f3 = tkFont.Font(size = 20, family = "Courier New")
+        f4 = tkFont.Font(size = 10, family = "Courier New")
         # 背景和圖片
         self.background = tk.Canvas(self, height = 600, width = 800, bg = 'white').pack()
         self.picture = tk.Canvas(self, height = 240, width = 240, bg = 'blue').place(x = 60, y = 60)
         # 經驗值
-        self.exp = tk.Label(self, text = "經驗：100", height = 1, width = 12, bg = 'white', font = f3).place(x = 445, y = 260)
+        self.exp = tk.Label(self, text = "經驗：10/100", height = 1, width = 12, bg = 'white', font = f3).place(x = 450, y = 290)
         # 首頁基礎數值
-        self.level = tk.Button(self, text = "等級：12", height = 1, width = 15, font = f3, anchor='w').place(x = 60, y = 360)
-        self.blood = tk.Button(self, text = "血量：231", height = 1, width = 15, font = f3, anchor='w').place(x = 60, y = 420)
-        self.attack = tk.Button(self, text = "攻擊：243", height = 1, width = 15, font =f3, anchor='w').place(x = 60, y = 480)
-        self.defend = tk.Button(self, text = "防禦：234", height = 1, width = 15, font =f3, anchor='w').place(x = 400, y = 360)
-        self.skill = tk.Button(self, text = "技能：5%攻擊加倍", height = 1, width = 15, font =f3, anchor='w').place(x = 400, y = 420)
-        self.dodge = tk.Button(self, text = "閃避：34%", height = 1, width = 15, font =f3, anchor='w').place(x = 400, y = 480)
+        self.level = tk.Button(self, text = "等級：12", height = 1, width = 15, bg='#ccdd69', font = f3, anchor='w').place(x = 60, y = 360)
+        self.blood = tk.Button(self, text = "血量：231", height = 1, width = 15, bg='#ccdd69', font = f3, anchor='w').place(x = 60, y = 420)
+        self.attack = tk.Button(self, text = "攻擊：243", height = 1, width = 15, bg='#ccdd69', font =f3, anchor='w').place(x = 60, y = 480)
+        self.defend = tk.Button(self, text = "防禦：234", height = 1, width = 15, bg='#ccdd69', font =f3, anchor='w').place(x = 400, y = 360)
+        self.skill = tk.Button(self, text = "技能：5%攻擊加倍", height = 1, width = 15, bg='#ccdd69', font =f3, anchor='w').place(x = 400, y = 420)
+        self.dodge = tk.Button(self, text = "閃避：34%", height = 1, width = 15, font =f3, bg='#ccdd69', anchor='w').place(x = 400, y = 480)
         # 角色名和自己的名稱
         self.charater_name = tk.Label(self, text = "桐人", height = 1, width = 10, bg = 'white', font =f1).place(x = 420 , y = 100)
-        self.name = tk. Label(self, text = "亞斯娜", height = 1, width = 10, bg = 'white', font =f2).place(x = 440 , y = 200)
+        self.name = tk. Label(self, text = StartPage.name, height = 1, width = 10, bg = 'white', font =f2).place(x = 80 , y = 310)
+        self.word = '桐谷和人（桐ヶ谷きりがや 和人かずと，Kirigaya Kazuto），虛擬世界的用戶名為「桐人（キリト，Kirito）」，是日本小說家川原礫輕小說《刀劍神域》及其改編動、漫畫作品的主角。在以《刀劍神域》為主的諸多系列作裡作為主角或重要角色登場。'
+        self.intro = tk.Message(self, text = self.word, font = f4, bg = 'white', width = 270).place(x = 400, y = 150)
         # 按鈕
-        self.boss = tk.Button(self, text = "關\n卡", height = 2, width = 2, font = f3, command= self.toBoss).place(x = 760 , y = 60)
-        self.character = tk.Button(self, text = "角\n色", height = 2, width = 2, font = f3).place(x = 760 , y = 180)
-        self.action  = tk.Button(self, text = "行\n動", height = 2, width = 2, font = f3, command= self.toAction).place(x = 760 , y = 300)
+        self.boss = tk.Button(self, text = "關\n卡", height = 2, width = 2, font = f3, bg='#bbaa69', command= self.toBoss).place(x = 760 , y = 60)
+        self.character = tk.Button(self, text = "角\n色", height = 2, width = 2, bg='#bbaa69', font = f3).place(x = 760 , y = 180)
+        self.action  = tk.Button(self, text = "行\n動", height = 2, width = 2, font = f3, bg='#bbaa69', command= self.toAction).place(x = 760 , y = 300)
     
     def toAction(self):
         self.destroy()
@@ -110,14 +116,14 @@ class Action(tk.Frame):
         self.output.place(x = 30, y = 300)
         # 按鈕
         self.back = tk.Button(self, text = "返回", height = 1, width = 4, font = f3, anchor='w', command= self.toGame).place(x = 30, y = 30)
-        self.a = tk.Button(self, text = "A", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext, 'a')).place(x = 35, y = 120)
-        self.b = tk.Button(self, text = "B", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'b')).place(x = 220, y = 120)
-        self.c = tk.Button(self, text = "C", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'c')).place(x = 405, y = 120)
-        self.d = tk.Button(self, text = "D", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'d')).place(x = 590, y = 120)
-        self.e = tk.Button(self, text = "E", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'e')).place(x = 35, y = 220)
-        self.f = tk.Button(self, text = "F", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'f')).place(x = 220, y = 220)
-        self.g = tk.Button(self, text = "G", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'g')).place(x = 405, y = 220)
-        self.h = tk.Button(self, text = "H", height = 1, width = 10, font = f3, anchor='w', command=partial(self.actiontext,'h')).place(x = 590, y = 220)
+        self.a = tk.Button(self, text = "A", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext, 'a')).place(x = 35, y = 120)
+        self.b = tk.Button(self, text = "B", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'b')).place(x = 220, y = 120)
+        self.c = tk.Button(self, text = "C", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'c')).place(x = 405, y = 120)
+        self.d = tk.Button(self, text = "D", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'d')).place(x = 590, y = 120)
+        self.e = tk.Button(self, text = "E", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'e')).place(x = 35, y = 220)
+        self.f = tk.Button(self, text = "F", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'f')).place(x = 220, y = 220)
+        self.g = tk.Button(self, text = "G", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'g')).place(x = 405, y = 220)
+        self.h = tk.Button(self, text = "H", height = 1, width = 10, font = f3, bg='#228922', anchor='w', command=partial(self.actiontext,'h')).place(x = 590, y = 220)
         # 標題
         self.title = tk.Label(self, text = "行動", height = 1, width = 10, bg = 'white', font = f1).place(x = 600 , y = 25)
         # CD
@@ -151,94 +157,96 @@ class Action(tk.Frame):
     def toGame(self):
         self.destroy()
         self.master.switch_frame(Game)
+
 class Boss(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self)
         self.grid()
-        self.master.geometry('800x600')
-        self.master.minsize(800, 600)
-        self.master.maxsize(800, 600)
         self.background()
         self.createWidgets()
-        self.master.configure(bg='white')
-        self.configure(bg='white')
+        self.master.minsize(800, 600)
+        self.master.maxsize(800, 600)
+
 
     def background(self):  # 暫定背景圖
-        self.imageBG = tk.PhotoImage(file='p6BG.gif')  # 注意檔案路徑
+        self.imageBG = tk.PhotoImage(file='p6BG1.GIF')  # 注意檔案路徑
         self.bg = tk.Label(self, image=self.imageBG)
-        self.bg.grid(row=0, column=0, columnspan=4)
+        self.bg.grid(row=1, column=0, columnspan=4)
 
     def createWidgets(self):
         pFile = 'p6P.gif'  # 注意檔案路徑，會隨上場角色更動所以分開寫
         self.imageP = tk.PhotoImage(file=pFile)  # 注意檔案路徑
         self.pImage = tk.Label(self, image=self.imageP)  # 玩家角色圖
-        self.pImage.grid(row=0, column=0, columnspan=2, sticky=tk.W)
+        self.pImage.grid(row=1, column=0, columnspan=2, sticky=tk.W)
 
         bFile = 'p6P.gif'  # 注意檔案路徑，會隨上場角色更動所以分開寫
         self.imageB = tk.PhotoImage(file=bFile)  # 注意檔案路徑
         self.bImage = tk.Label(self, image=self.imageB)  # Boss角色名
-        self.bImage.grid(row=0, column=3, columnspan=2, sticky=tk.E)
+        self.bImage.grid(row=1, column=2, columnspan=2, sticky=tk.E)
 
         fSize1 = tkFont.Font(size=20)
         pName = "玩家角色名"  # 會隨上場角色更動所以分開寫
         self.nameP = tk.Label(self, text=pName, font=fSize1)
-        self.nameP.grid(row=0, column=0, sticky=tk.S)
+        self.nameP.grid(row=2, column=0, sticky=tk.S)
         bName = "Boss角色名"  # 會隨上場角色更動所以分開寫
         self.nameB = tk.Label(self, text=bName, font=fSize1)
-        self.nameB.grid(row=0, column=3, sticky=tk.S)
+        self.nameB.grid(row=2, column=3, sticky=tk.S)
 
         fSize2 = tkFont.Font(size=14)
         pLevel = 0  # 會隨上場角色更動所以分開寫
         self.level = tk.Label(self, text=("Lv.", pLevel), font=fSize2)  # 角色等級
-        self.level.grid(row=0, column=1, sticky=tk.SW)
+        self.level.grid(row=2, column=1, sticky=tk.SW)
 
         pInfo = [0, 1, 2, 3, 4]  # 玩家角色資訊[血量,攻擊,防禦,技能,閃避]
         bInfo = [0.0, 0.1, 0.2, 0.3, 0.4]  # Boss角色資訊[血量,攻擊,防禦,技能,閃避]
 
-        self.hp = tk.Label(self, text="－ －血量－ －", height=2, font=fSize2, bg='white')
-        self.hp.grid(row=1, column=1, columnspan=2)
-        self.p_hp = tk.Label(self, text=str(pInfo[0]), height=2, font=fSize2, bg='white')
-        self.p_hp.grid(row=1, column=0)
-        self.b_hp = tk.Label(self, text=str(bInfo[0]), height=2, font=fSize2, bg='white')
-        self.b_hp.grid(row=1, column=3)
+        self.hp = tk.Label(self, text="－ －血量－ －", height=2, font=fSize2, bg='yellow')
+        self.hp.grid(row=4, column=1, columnspan=2)
+        self.p_hp = tk.Label(self, text=str(pInfo[0]), height=2, font=fSize2, bg='yellow')
+        self.p_hp.grid(row=4, column=0)
+        self.b_hp = tk.Label(self, text=str(bInfo[0]), height=2, font=fSize2, bg='yellow')
+        self.b_hp.grid(row=4, column=3)
 
-        self.atk = tk.Label(self, text="－ －攻擊－ －", height=2, font=fSize2, bg='white')
-        self.atk.grid(row=2, column=1, columnspan=2)
-        self.p_atk = tk.Label(self, text=str(pInfo[1]), height=2, font=fSize2, bg='white')
-        self.p_atk.grid(row=2, column=0)
-        self.b_atk = tk.Label(self, text=str(bInfo[1]), height=2, font=fSize2, bg='white')
-        self.b_atk.grid(row=2, column=3)
+        self.atk = tk.Label(self, text="－ －攻擊－ －", height=2, font=fSize2, bg='yellow')
+        self.atk.grid(row=5, column=1, columnspan=2)
+        self.p_atk = tk.Label(self, text=str(pInfo[1]), height=2, font=fSize2, bg='yellow')
+        self.p_atk.grid(row=5, column=0)
+        self.b_atk = tk.Label(self, text=str(bInfo[1]), height=2, font=fSize2, bg='yellow')
+        self.b_atk.grid(row=5, column=3)
 
-        self.defend = tk.Label(self, text="－ －防禦－ －", height=2, font=fSize2, bg='white')
-        self.defend.grid(row=3, column=1, columnspan=2)
-        self.p_defend = tk.Label(self, text=str(pInfo[2]), height=2, font=fSize2, bg='white')
-        self.p_defend.grid(row=3, column=0)
-        self.b_defend = tk.Label(self, text=str(bInfo[2]), height=2, font=fSize2, bg='white')
-        self.b_defend.grid(row=3, column=3)
+        self.defend = tk.Label(self, text="－ －防禦－ －", height=2, font=fSize2, bg='yellow')
+        self.defend.grid(row=6, column=1, columnspan=2)
+        self.p_defend = tk.Label(self, text=str(pInfo[2]), height=2, font=fSize2, bg='yellow')
+        self.p_defend.grid(row=6, column=0)
+        self.b_defend = tk.Label(self, text=str(bInfo[2]), height=2, font=fSize2, bg='yellow')
+        self.b_defend.grid(row=6, column=3)
 
-        self.skill = tk.Label(self, text="－ －技能－ －", height=2, font=fSize2, bg='white')
-        self.skill.grid(row=4, column=1, columnspan=2)
-        self.p_skill = tk.Label(self, text=str(pInfo[3]), height=2, font=fSize2, bg='white')
-        self.p_skill.grid(row=4, column=0)
-        self.b_skill = tk.Label(self, text=str(bInfo[3]), height=2, font=fSize2, bg='white')
-        self.b_skill.grid(row=4, column=3)
+        self.skill = tk.Label(self, text="－ －技能－ －", height=2, font=fSize2, bg='yellow')
+        self.skill.grid(row=7, column=1, columnspan=2)
+        self.p_skill = tk.Label(self, text=str(pInfo[3]), height=2, font=fSize2, bg='yellow')
+        self.p_skill.grid(row=7, column=0)
+        self.b_skill = tk.Label(self, text=str(bInfo[3]), height=2, font=fSize2, bg='yellow')
+        self.b_skill.grid(row=7, column=3)
 
-        self.miss = tk.Label(self, text="－ －閃避－ －", height=2, font=fSize2, bg='white')
-        self.miss.grid(row=5, column=1, columnspan=2)
-        self.p_miss = tk.Label(self, text=str(pInfo[4]), height=2, font=fSize2, bg='white')
-        self.p_miss.grid(row=5, column=0)
-        self.b_miss = tk.Label(self, text=str(bInfo[4]), height=2, font=fSize2, bg='white')
-        self.b_miss.grid(row=5, column=3)
+        self.miss = tk.Label(self, text="－ －閃避－ －", height=2, font=fSize2, bg='yellow')
+        self.miss.grid(row=8, column=1, columnspan=2)
+        self.p_miss = tk.Label(self, text=str(pInfo[4]), height=2, font=fSize2, bg='yellow')
+        self.p_miss.grid(row=8, column=0)
+        self.b_miss = tk.Label(self, text=str(bInfo[4]), height=2, font=fSize2, bg='yellow')
+        self.b_miss.grid(row=8, column=3)
 
         fSize3 = tkFont.Font(size=12)
-        self.btnBack = tk.Button(self, text="BACK", height=2, width=8, font=fSize3, command= self.toGame)  # 暫定返回鍵
+        self.btnBack = tk.Button(self, text="BACK", height=2, width=8, font=fSize3, command=self.toGame)  # 暫定返回鍵
         self.btnBack.grid(row=0, column=0, sticky=tk.NW)
 
-        self.btnFight = tk.Button(self, text="戰  鬥", height=2, width=10, font=fSize1)  # 暫定戰鬥鍵
-        self.btnFight.grid(row=6, column=1, columnspan=2)
+        self.btnFight = tk.Button(self, text="戰  鬥", height=2, width=10, font=fSize1, bg='#FF5151')  # 暫定戰鬥鍵
+        self.btnFight.grid(row=9, column=1, columnspan=2)
+
+
     def toGame(self):
         self.destroy()
         self.master.switch_frame(Game)
+
 if __name__ == "__main__":
     app = SampleApp()
     app.mainloop()
